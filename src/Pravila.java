1e04e49837e93b77b3f52d7b9ac73c68a6eb9dec
barbara7777 <badobe.lipnik@gmail.com>
2019-05-10 20:27:56 +0200

    Naredila premikanje v fazi dve

--- src/Pravila.java
+++ src/Pravila.java
@@ -35,44 +35,4 @@ public class Pravila {
 		}
 		return true;			
 	}
-		
-	
-	
-	
-	
-	
-	// veljavnost potez
-	public static boolean jeVeljavna(Igralec player, Poteza poteza, Igra g) {
-		if(poteza.zacetno == poteza.koncno) {
-			System.out.println("Moraš se premakniti.");
-			return false;
-		}
-		if (player.faza == 1 || player.faza == 3) {
-			if  (poteza.koncno.zasedenost != Polje.prazno) return false;			
-		}
-		else if (player.faza == 2) {
-			// èe konèno ni prazno ali nista povezana, return false
-			if (poteza.koncno.zasedenost != Polje.prazno || 
-				   !IgralnaPloscaInfo.staPovezana(poteza.zacetno, poteza.koncno)) {
-				System.out.println("Ni prazno ali pa je predaleè.");
-				return false;			
-			}
-		}
-		// èe imamo za jemati
-		if (poteza.vzemi != null) {
-			if (poteza.vzemi.zasedenost == Polje.prazno) 
-				System.out.println("Na tem polju ni plošèka.");
-			
-			if (!g.jeNasprotnikovo(poteza.vzemi)) {
-				System.out.println("Tega plošèka ne moreš vzeti!");
-				return false;
-			}
-		}
-		return true; // èe je vse v redu, imamo veljavno potezo
-	}
 }
-/* zadnji del bi lahko na krajše napisali:
- *  if (poteza.vzemi != null)
- *  	return (poteza.vzemi.zasedenost == g.nasprotnik.ime);
- *  */
-
